f0b6c7d34df38e9c20e7d3c94be0dbd30d4da924
Michael Mangus <mmangus@gmail.com>
2023-06-14 11:27:10 -0700

    Test transitioning to recommended package format

--- co.skillprint.sdk/Runtime/Scripts/SkillprintCore.cs
+++ co.skillprint.sdk/Runtime/Scripts/SkillprintCore.cs
@@ -1,30 +1,10 @@
-/*
- * Copyright (C) 2012 GREE, Inc.
- * 
- * This software is provided 'as-is', without any express or implied
- * warranty.  In no event will the authors be held liable for any damages
- * arising from the use of this software.
- * 
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- * 
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
- */
-// ^ Note that this version of the source _has_ been altered by Skillprint and is _not_ the original software. ^  
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
 // FIXME this reference works in the Unity editor but Rider can't resolve it
 using Newtonsoft.Json;
 using UnityEngine;
+
 #if UNITY_2018_4_OR_NEWER
 using UnityEngine.Networking;
 #endif
